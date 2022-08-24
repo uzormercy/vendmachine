ea9cee603f945665b1248fa25bac67d72ff89be8
Uzor Mercy <uzormercies@gmail.com>
2022-08-24 01:47:26 +0100

    Minor Fix

--- backend/src/services/UserService.js
+++ backend/src/services/UserService.js
@@ -1,4 +1,17 @@
-import { findOne } from '../utils/execute';
+import * as R from 'ramda';
+import { findAndUpdate, findOne } from '../utils/execute';
 import User from '../models/User';
 
 export const getUser = (_id) => findOne(User)({ _id });
+
+export const updateUser = (data) => {
+  if (R.isEmpty(data.fullname)) {
+    return {
+      status: 422,
+      message: 'Fullname is required'
+    };
+  }
+  return findAndUpdate(User)(data.id)(data.fullname);
+};
+
+export const updateUserRole = (data) => {};
